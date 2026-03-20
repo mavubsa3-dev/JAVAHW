81790fc4bf41005fb17c9470ae24a49c26e8f0a9
mavubsa3-dev <mavubsa3@gmail.com>
2026-03-20 15:59:06 +0900

    enum 작성

--- ArithmeticCalculator.java
+++ ArithmeticCalculator.java
@@ -1,2 +1,15 @@
 public class ArithmeticCalculator {
+    public enum OperatorType {
+        SUM('+'),
+        MIN('-'),
+        MUL('*'),
+        DIV('%');
+
+        private final char sign;
+
+        OperatorType(char sign) {
+            this.sign = sign;
+        }
+    }
+
 }
