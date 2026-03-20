d8c82557554d6670a88b7667af5c2ddb4a93f38d
mavubsa3-dev <mavubsa3@gmail.com>
2026-03-20 17:06:51 +0900

    enum 활용해 배열 선언 및 연산 기호 정의

--- ArithmeticCalculator.java
+++ ArithmeticCalculator.java
@@ -1,15 +1,40 @@
 public class ArithmeticCalculator {
+
     public enum OperatorType {
-        SUM('+'),
-        MIN('-'),
-        MUL('*'),
-        DIV('%');
+        SUM("+"),
+        MIN("-"),
+        MUL("*"),
+        DIV("%");
 
-        private final char sign;
+        private final String sign;
 
-        OperatorType(char sign) {
+        OperatorType(String sign) {
             this.sign = sign;
+        }
+
+        public String getSign() {
+            return sign;
         }
     }
 
+    public void Calculate(int num1, int num2, String sign)
+    {
+        OperatorType[] types = {OperatorType.SUM, OperatorType.MIN, OperatorType.MUL, OperatorType.DIV};
+        String mathtype = null;
+        int result = 0;
+        for(int i=0; i<OperatorType.values().length; i++)
+            {
+                String type = types[i].getSign();
+                if (type == sign)
+                {
+                    mathtype = sign;
+                }
+            }
+        switch (mathtype)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+        }
+    }
 }
