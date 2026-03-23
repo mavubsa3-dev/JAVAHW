50065f3f5d67d51f26ea4358edd0dea68e0d8989
mavubsa3-dev <mavubsa3@gmail.com>
2026-03-23 20:20:39 +0900

    List 접근 제한자 수정

--- CalculatorRep.java
+++ CalculatorRep.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CalculatorRep{
-    List<ArithmeticCalculator<?>> list = new ArrayList<>();
+     private List<ArithmeticCalculator<?>> list = new ArrayList<>();
 
     public void add(ArithmeticCalculator<?> resultlist)
     {
